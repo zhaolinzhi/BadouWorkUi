dc2d21286f0fe5b7487ce464c7c5c76d001f31ba
zhaolinzhi <zhaolinzhi@badousoft.com>
2026-09-14 12:06:48 +0800

    fix(sider): narrow collapsed rail from 72px to 64px

--- packages/desktop/src/renderer/components/layout/Layout.tsx
+++ packages/desktop/src/renderer/components/layout/Layout.tsx
@@ -95,7 +95,7 @@ const useDebug = () => {
 const UpdateModal = React.lazy(() => import('@/renderer/components/settings/UpdateModal'));
 
 const DEFAULT_SIDER_WIDTH = 260;
-const DESKTOP_COLLAPSED_WIDTH = 72;
+const DESKTOP_COLLAPSED_WIDTH = 64;
 const SIDER_DRAG_SNAP_THRESHOLD = Math.round((DEFAULT_SIDER_WIDTH + DESKTOP_COLLAPSED_WIDTH) / 2);
 const SIDER_DRAG_HYSTERESIS = 6;
 const MOBILE_SIDER_WIDTH_RATIO = 0.67;
@@ -417,7 +417,7 @@ const Layout: React.FC<{
 
           <ArcoLayout className={'size-full layout flex-1 min-h-0'}>
             <ArcoLayout.Sider
-              collapsedWidth={isMobile ? 0 : 72}
+              collapsedWidth={isMobile ? 0 : 64}
               collapsed={collapsed}
               width={siderWidth}
               className={classNames('!bg-2 layout-sider', {
